8751298617cb7a5c0400828d554e7b9de232005e
eduardosolorzano92 <45920825+eduardosolorzano92@users.noreply.github.com>
2025-02-18 18:08:40 -0600

    Update main.tf
    Forth Commit

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 
 resource "aws_security_group" "sg" {
-    name        = "allow_tls-${count.index}"
+    name        = "allow_tls"
     description = "Allow TLS inbound traffic and all outbound traffic"
-    count = 1
 }
