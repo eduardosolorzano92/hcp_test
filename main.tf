00af623af13c03100964702745e5cc3af7751b9f
eduardosolorzano92 <45920825+eduardosolorzano92@users.noreply.github.com>
2025-02-18 18:06:40 -0600

    Update main.tf
    Third Commit

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = "1.9.1"
-
-  required_providers {
-    aws = {
-      version = "5.54.1"
-      source = "hashicorp/aws"
-    }
-  }
-}
 
 resource "aws_security_group" "sg" {
     name        = "allow_tls-${count.index}"
